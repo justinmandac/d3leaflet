f7d7fdeceb2b2a372b9a1e3da88858d3dd9507a7
Bien Mandac <justinmandac@gmail.com>
2017-08-17 15:25:14 +0800

    Try

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@
  * @fileoverview Entry point.
 */
 const d3 = require('d3');
-const leaflet = require('leaflet');
 const URL = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-const map = new L.Map('map', {center: [37.8, -96.9], zoom: 4});
-const tileLayer = new L.TileLayer(URL);
-
-map.addLayer(tileLayer)
-   .setView([14.5995, 120.9842], 5); // center to MNL, PH
+// Center to MNL PH
+console.log(L);
+const map = new L.Map('map', {center: [14.5995, 120.9842], zoom: 5});
+const gl = L.MapboxGL({
+  accessToken : 'pk.eyJ1IjoianVzdGlubWFuZGFjIiwiYSI6ImNqNmczc3FyajA1NHgycHBrNzlneW9oMG0ifQ.eF_q_8CkR4-vlGhBAMqhtw',
+  style: 'mapbox://styles/mapbox/dark-v9',
+}).addTo(map);
 
 const svg = d3.select(map.getPanes().overlayPane).append('svg');
 const g = svg.append('g').attr('class', 'leaflet-zoom-hide');
