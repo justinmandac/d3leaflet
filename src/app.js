d1c551473eb5176f3c0045bfb43337956e7843f1
Bien Mandac <justinmandac@gmail.com>
2017-08-17 14:40:10 +0800

    Center to Manila

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@
  * @fileoverview Entry point.
 */
 const leaflet = require('leaflet');
-const mapContainer = document.getElementById('map');
+const map = new L.Map('map', {center: [37.8, -96.9], zoom: 4});
+const tileLayer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-const map = new L.Map('map', {center: [37.8, -96.9], zoom: 4})
-.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
+map.addLayer(tileLayer)
+   .setView([14.5995, 120.9842], 5); // center to MNL, PH
