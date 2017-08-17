2908999c9ebf4a216f4bd0c41ad19e02b982afc0
Bien Mandac <justinmandac@gmail.com>
2017-08-17 14:36:07 +0800

    Made Leaflet work

--- src/app.js
+++ src/app.js
@@ -1,2 +1,8 @@
-const a = 22;
-console.log(a);
+/**
+ * @fileoverview Entry point.
+*/
+const leaflet = require('leaflet');
+const mapContainer = document.getElementById('map');
+
+const map = new L.Map('map', {center: [37.8, -96.9], zoom: 4})
+.addLayer(new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'));
