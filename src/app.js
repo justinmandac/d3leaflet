60efe729eb05509eb6883f304ca25862a24d88e3
Bien Mandac <justinmandac@gmail.com>
2017-08-18 10:01:34 +0800

    Created a separate class for the West Valley Fault layer

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@
 import * as d3 from 'd3';
 import L from 'leaflet';
 import 'mapbox-gl-leaflet/leaflet-mapbox-gl';
+import WestValleyLayer from './west-valley.layer';
 
 // Center to MNL PH
 const map = new L.Map('map', {center: [14.5995, 120.9842], zoom: 8});
@@ -12,62 +13,6 @@ const gl = (new L.MapboxGL({
   style: 'mapbox://styles/mapbox/dark-v9',
 }))
 .addTo(map);
+const westValleyLayer = new WestValleyLayer(L, map, d3);
 
-console.log(map);
-
-const svg = d3.select(map.getPanes().overlayPane).append('svg');
-const g = svg.append('g').attr('class', 'leaflet-zoom-hide');
-
-function projectPoint(x, y) {
-  const point = map.latLngToLayerPoint(new L.LatLng(y, x));
-  this.stream.point(point.x, point.y);
-}
-let path;
-let data;
-let feature;
-
-function reset() {
-  const bounds = path.bounds(data);
-  const topLeft = bounds[0];
-  const bottomRight = bounds[1];
-
-  svg.attr("width", bottomRight[0] - topLeft[0])
-  .attr("height", bottomRight[1] - topLeft[1])
-  .style("left", topLeft[0] + "px")
-  .style("top", topLeft[1] + "px");
-
-  g.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
-  feature.attr('d', path);
-}
-
-map.on("zoomend ", reset);
-
-d3.request('data/west-valley-fault.geojson', (err, {response}) => {
-  if (!!err) {
-    console.error(err);
-    return;
-  }
-
-  data = JSON.parse(response);
-  const transform = d3.geoTransform({
-    point(x, y) {
-      const point = map.latLngToLayerPoint(new L.LatLng(y, x));
-      this.stream.point(point.x, point.y);
-    },
-  });
-
-  path = d3.geoPath(transform);
-
-  console.log(path);
-  feature =
-    g.selectAll('path')
-      .data(data.features)
-      .enter()
-      .append('path')
-      .attr("d", path) //fill="orange" stroke="black" stroke-width="3"
-      .attr("stroke-width", '2')
-      .attr("stroke", 'orange');
-
-  reset();
-
-});
+westValleyLayer.load();
---
+++ src/west-valley.layer.js
@@ -0,0 +1,79 @@
+/**
+ * @fileoverview Layer Class for the West Valley Fault.
+ */
+export default class WestValleyLayer {
+  constructor(leaflet, map, d3, dataUrl = 'data/west-valley-fault.geojson') {
+    console.log(arguments);
+    this.data = {};
+    this.feature = {};
+
+    this.path_ = () => {};
+    this.d3_ = d3;
+    this.map_ = map;
+    this.dataUrl_ = dataUrl;
+    this.leaflet_ = leaflet;
+
+    this.svg_ =
+      d3.select(this.map_.getPanes().overlayPane)
+        .append('svg');
+    this.g_ =
+      this.svg_.append('g').attr('class', 'leaflet-zoom-hide wvf-layer');
+
+    this.map_.on('zoomend', () => this.reset());
+    this.isHidden_ = false;
+  }
+
+  get isHidden() { return this.isHidden_; }
+
+  toggle() {
+    this.isHidden_ = !this.isHidden_;
+  }
+
+  /** Loads WVF coordinates. */
+  load() {
+    return new Promise((resolve, reject) => {
+      this.d3_.request(this.dataUrl_, (err, {response}) => {
+        if (!!err) {
+          reject(err);
+          return;
+        }
+
+        this.data = JSON.parse(response);
+        const map_ = this.map_;
+        const L_ = this.leaflet_;
+        const transform = this.d3_.geoTransform({
+          point(x, y) {
+            const point = map_.latLngToLayerPoint(new L_.LatLng(y, x));
+            this.stream.point(point.x, point.y);
+          },
+        });
+        this.path_ = this.d3_.geoPath(transform);
+        this.feature =
+        this.g_.selectAll('path')
+          .data(this.data.features)
+          .enter()
+          .append('path')
+          .attr("d", this.path_) //fill="orange" stroke="black" stroke-width="3"
+          .attr("stroke-width", '2')
+          .attr("stroke", 'orange');
+
+        this.reset();
+        resolve();
+      });
+    });
+  }
+
+  reset() {
+    const bounds = this.path_.bounds(this.data);
+    const topLeft = bounds[0];
+    const bottomRight = bounds[1];
+
+    this.svg_.attr("width", bottomRight[0] - topLeft[0])
+      .attr("height", bottomRight[1] - topLeft[1])
+      .style("left", topLeft[0] + "px")
+      .style("top", topLeft[1] + "px");
+
+    this.g_.attr("transform", "translate(" + -topLeft[0] + "," + -topLeft[1] + ")");
+    this.feature.attr('d', this.path_);
+  }
+}
